696e1bee01309462e4c605bddbb445e5ee1b24c2
xertrex_cp <SND\xertrex_cp>
2010-05-20 00:04:18 -0700

    Added test

---
+++ XmlSearchAndReplace/XmlSnPTest/XmlSearchReplaceLibTest/AbstractReplacerEngineTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XmlSearchReplaceLib.Engine;
+
+namespace XmlSnRTest.XmlSearchReplaceLibTest
+{
+    /// <summary>
+    /// Summary description for AbstractReplacerEngineTest
+    /// </summary>
+    [TestClass]
+    public class AbstractReplacerEngineTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateEngine_WithRegExEngine_ThrowsArgumentException()
+        {
+            AbstractReplacerEngine.CreateEngine(XmlSearchReplaceLib.ReplacerEngineType.RegexEngine, XmlSearchReplaceLib.SearchReplaceOperationOptions.CaseInsensitive);
+        }
+    }
+}
